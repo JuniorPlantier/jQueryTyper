e1fb676744aedb14b500394528fac9dcc2780f0b
Junior Plantier <junior.plantier@gmail.com>
2019-06-22 12:08:56 -0300

    construindo o botao de reiniciar e estruturando o código

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,53 @@
-var frase = $(".frase").text();
-var nroPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase").text(nroPalavras);
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $('.campo-digitacao');
+
+//$(document).ready(function)
+$( function() {
+        atualizaTamanhoFrase();
+        inicializaContadores();
+        inicializaCronometro();
+        $("#botao-reiniciar").click(reiniciarJogo);
+});
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var nroPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase").text(nroPalavras);
+}
+
+
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+    
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras); 
+    
+        var qtdCaracteres = conteudo.length;
+        $('#contador-caracteres').text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function() {
+        var cronometroId = setInterval(function() {
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroId);
+            }
+        }, 1000);
+    });
+}
+
+function reiniciarJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+}
